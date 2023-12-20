6ab66f5888e3ceb10613fde66d5f42b6e25a6e85
LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
2023-12-20 22:00:25 +0100

    aanpassingen

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
- hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
+document.querySelectorAll(".nav-link").forEach(n =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+document.querySelectorAll(".idee_box").forEach(function (box) {
+  box.addEventListener("click", function () {
+    // Navigeer naar de pagina "idee.html"
+    window.location.href = "idee.html";
+  });
+});
